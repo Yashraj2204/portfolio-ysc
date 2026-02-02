36c5d23eee16e6ab422fd0a35af0441ea1f3f841
Yashraj2204 <yashrajsinghchauhan03@gmail.com>
2026-02-02 16:36:31 +0530

    Fix case-sensitivity issues in imports for Vercel deployment

--- src/sections/experience.jsx
+++ src/sections/experience.jsx
@@ -1,4 +1,4 @@
-import { Timeline } from "../components/TimeLine";
+import { Timeline } from "../components/timeline";
 import { experiences } from "../constants";
 const Experiences = () => {
   return (
